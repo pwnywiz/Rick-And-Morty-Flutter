2ec6abe0f89076ced08647899647b423a2a345d8
Ignatios <pwnywiz@gmail.com>
2019-04-18 17:28:47 +0300

    First working version

--- lib/api/character_repository.dart
+++ lib/api/character_repository.dart
@@ -2,12 +2,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:my_app/models/character_page.dart';
 
-Future<CharacterPage> fetchCharacters() async {
-  final response = await http.get('https://rickandmortyapi.com/api/character/');
+class CharacterAPI {
+  Future<CharacterPage> fetchCharacters(String url) async {
+    final response = await http.get(url);
 
-  if (response.statusCode == 200) {
-    return CharacterPage.fromJson(json.decode(response.body));
-  } else {
-    throw Exception('Failed to load post');
+    if (response.statusCode == 200) {
+      return CharacterPage.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load post');
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
 
 import 'package:my_app/api/character_repository.dart';
+import 'package:my_app/models/character.dart';
 import 'package:my_app/models/character_page.dart';
 
+void main() => runApp(MyApp(characterAPI: CharacterAPI()));
 
-void main() => runApp(MyApp(characterPage: fetchCharacters()));
+class MyApp extends StatefulWidget {
+  final CharacterAPI characterAPI;
 
-class MyApp extends StatelessWidget {
-  final Future<CharacterPage> characterPage;
+  MyApp({Key key, this.characterAPI}) : super(key: key);
 
-  MyApp({Key key, this.characterPage}) : super(key: key);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ScrollController _scrollController = ScrollController();
+  List<Character> characters = [];
+  CharacterPage characterPage;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData('https://rickandmortyapi.com/api/character/');
+
+    _scrollController.addListener(() {
+      ScrollPosition scrollPosition = _scrollController.position;
+      if (scrollPosition.pixels == scrollPosition.maxScrollExtent) {
+        print(_scrollController.position.pixels);
+        fetchData(characterPage.next);
+        print('Fetched $characterPage');
+      }
+    });
+  }
+
+  void fetchData(String url) async {
+    characterPage = await widget.characterAPI.fetchCharacters(url);
+    setState(() {
+      characters.addAll(characterPage.characterList);
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,23 +56,75 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Rick And Morty DB'),
-        ),
-        body: Center(
-          child: FutureBuilder<CharacterPage>(
-            future: characterPage,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.characterList[0].name);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-
-              return CircularProgressIndicator();
-            },
+          appBar: AppBar(
+            title: Center(child: Text('Rick And Morty DB')),
           ),
-        ),
+          body: _createListView()),
+    );
+  }
+
+  Widget _createListView() {
+    return ListView.builder(
+      itemCount: characters.length,
+      controller: _scrollController,
+      itemBuilder: (context, index) {
+        return Hero(
+            tag: characters[index].name,
+            child: GestureDetector(
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => DetailsPage(
+                            character: characters[index],
+                          ))),
+              child: Container(
+                child: Image.network(
+                  characters[index].image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ));
+      },
+    );
+  }
+}
+
+class DetailsPage extends StatelessWidget {
+  final Character character;
+
+  DetailsPage({Key key, @required this.character}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(character.name),
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Hero(
+                  tag: character.name,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(character.image),
+                    radius: 200,
+                  ),
+                ),
+              )
+            ],
+          ),
+          Text(
+            character.name,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black.withOpacity(0.6),
+                height: 5.0,
+                fontSize: 25),
+          ),
+          Text(character.species, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.cyan.withOpacity(0.8)),),
+        ],
       ),
     );
   }
